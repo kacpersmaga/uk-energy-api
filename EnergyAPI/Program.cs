08d3d9afc3310f6b6eb1eebcecfbac9f41996810
Kacper Smaga <kacper.smaga@microsoft.wsei.edu.pl>
2026-06-10 13:42:00 +0200

    feat: add cors policy for frontend

--- EnergyAPI/Program.cs
+++ EnergyAPI/Program.cs
@@ -7,9 +7,19 @@ builder.Services.AddScoped<IEnergyMixService, EnergyMixService>();
 builder.Services.AddScoped<IChargingWindowService, ChargingWindowService>();
 builder.Services.AddControllers();
 
+var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>() ?? [];
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+        policy.WithOrigins(allowedOrigins)
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseCors("AllowFrontend");
 app.MapControllers();
 
 app.Run();
